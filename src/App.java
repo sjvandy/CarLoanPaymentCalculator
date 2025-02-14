d14f670fe0161af20e55751a8a8496c923bff3d1
Steven <Vandegrift>
2025-02-14 00:13:55 -0600

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int carLoan = 10_000;
+        int loanLength = 3;
+        int interestRate = 5;
+        int downPayment = 2000;
+        if (loanLength <= 0 || interestRate <= 0) {
+            System.out.println("Error! You must take out a valid car loan.");            
+        } else if (downPayment >= carLoan) {
+            System.out.println("You can purchase the car outright!");
+        } else {
+            int remainingBalace = carLoan - downPayment;
+            int months = loanLength * 12;
+            int monthlyBalace = remainingBalace / months;
+            int interest = monthlyBalace * interestRate / 100;
+            int monthlyPayment = monthlyBalace + interest;
+            System.out.println("Your monthly payment will be $" + monthlyPayment + " a month.");
+        }
     }
 }
